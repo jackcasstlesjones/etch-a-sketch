3f6eb9fadbc6f19a678d6e51bd79b06d89d7705a
Jack Casstles-Jones <jackcasstlesjones@gmail.com>
2023-08-11 17:05:34 +1000

    Added colour-changing functionality, added user choosing number of divs via prompt

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
 const container = document.querySelector(".container");
-
+const askUserNumber = function () {
+  return prompt("How many squares would you like?");
+};
+const userNumber = askUserNumber();
 const createDivs = function () {
-  for (let i = 1; i < 17; i++) {
+  for (let i = 1; i < userNumber; i++) {
     let newDiv = document.createElement("div");
     container.appendChild(newDiv);
-    newDiv.textContent = "rat";
+    newDiv.classList.add("grid-divs");
+    newDiv.style.height = "";
+
+    newDiv.addEventListener("mouseover", function () {
+      newDiv.classList.add("pink");
+    });
   }
 };
 
