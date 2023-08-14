d0b0b7f65d4f6a6549bf2bbe32eb9cf4e65efc69
Jack Casstles-Jones <jackcasstlesjones@gmail.com>
2023-08-14 14:12:17 +1000

    Added border, added a button to ask the amount of squares, gave divs percentage size

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
 const container = document.querySelector(".container");
-const askUserNumber = function () {
-  return prompt("How many squares would you like?");
-};
-const userNumber = askUserNumber();
-const createDivs = function () {
-  for (let i = 1; i < userNumber; i++) {
+const button = document.getElementById("button");
+let userSquares;
+
+button.addEventListener("click", function () {
+  const userNumber = prompt("How many squares would you like?");
+  if (userNumber >= 100) {
+    return alert("Please pick a number lower than 100");
+  }
+  container.innerHTML = "";
+  console.log(userNumber * userNumber);
+  createDivs(userNumber * userNumber);
+});
+
+const createDivs = function (divsNumber) {
+  for (let i = 1; i <= divsNumber; i++) {
+    console.log(divsNumber);
     let newDiv = document.createElement("div");
     container.appendChild(newDiv);
     newDiv.classList.add("grid-divs");
@@ -16,4 +26,4 @@ const createDivs = function () {
   }
 };
 
-createDivs();
+createDivs(256);
